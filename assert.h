f13398ac3f5255704705d86027f92e92b3a01092
KailasDev <kailas3476@gmail.com>
2015-07-28 20:25:34 +0200

    Update assert.h

--- assert.h
+++ assert.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-#define c_assert(cond)      if (cond == false) { fprintf(err, "%s line %s : %s", __FILE__, __LINE__ ,#cond); }
-#define p_assert(pointer)   if (pointer == NULL) { fprintf(err, "%s line %s : %s == NULL", __FILE__, __LINE__ ,#pointer); }
+#define c_assert(cond)      if (cond == false) { fprintf(stderr, "%s line %d : %s\n", __FILE__, __LINE__ ,#cond); }
+#define p_assert(pointer)   if (pointer == NULL) { fprintf(stderr, "%s line %d : %s == NULL\n", __FILE__, __LINE__ ,#pointer); }
 
 #endif
